119a821402210211422d0f9ae4e4dc94cdc5ee7e
Alexandra Nava <aenava@ucsc.edu>
2024-01-22 15:58:55 -0800

    Completed Device Plots

--- file_count_plot.py
+++ file_count_plot.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import matplotlib.dates as mdates
 
 plt.rcParams['font.family'] = 'Arial'
 
 
-def plot_data(key, data):
+def plot_data(key, data, type_):
+    date = ""
+    count = ""
     dates = []
     counts = []
     for row in data:
@@ -22,32 +25,43 @@ def plot_data(key, data):
     # Reorder dates and counts based on sorted indices
     dates = [dates[i] for i in sorted_indices]
     counts = [counts[i] for i in sorted_indices]
-    print(dates)
-    print(counts)
-
-    plt.plot(dates, counts)
 
     plt.plot(dates, counts)
     title = key.replace(" ", "")
-    title = title.replace("Exp", "Experiment ")
-    title = title.replace("Pig", " ("+ type_ +"): Pig ")
+    title = title.replace("Exp", type_ + ": Experiment ")
+    title = title.replace("Pig",  ", Pig ")
     title = title.replace("Wound", ", Wound ")
     title = title.replace("_", "")
     title = title.replace("-", "")
     plt.title(title)
-    plt.xlabel("Year_Month_Day_Hour_Minute")
+    plt.xlabel("Time")
     plt.ylabel("Number of Images")
     plt.subplots_adjust(bottom=0.2)
 
+    date_formatter = mdates.DateFormatter("%Y-%m-%d %H:%M")
+    plt.gca().xaxis.set_major_formatter(date_formatter)
+
     plt.xticks(fontsize = 8, rotation = 40, ha='right')
-    plt.yticks(range(min(counts), max(counts)+1), fontsize = 10)
+    plt.yticks(range(min(counts), max(counts)+1), fontsize = 8)
+    plt.grid(axis='y', linestyle='dotted', alpha=0.5)
+
     plt.savefig(base + "Plots/" + type_ + "_" + key.strip())
+    plt.close() 
+    # for x, y in zip(dates,counts):
+    #     plt.text(x, y, f'({x}, {y})', ha='right', va='bottom', fontsize = 5, rotation = 20)  # Display coordinates as labels
+
+    #debugging:
+    # df = pd.DataFrame({
+    #     "Dates": dates,
+    #     "Count": counts
+    # })
+    # df.to_csv(base + "/Plots/Individual Data/" + title + ".csv")
 
 if __name__ == "__main__":
   global base
   type_ = "Device"
   base =  "/Users/alexandranava/Desktop/DARPA/Tasks/Count_Control_Images/Count Data/" 
-  file = "Data_V3.csv"
+  file = "/Data_" + type_ + "_V3.csv"
   path = base + file 
 
   exps_found = {}
@@ -64,6 +78,6 @@ if __name__ == "__main__":
 
   for key in exps_found.keys():
       data = exps_found[key]
-      print("------------------------------Plotting " + key + "...")
-      plot_data(key, data)
+      print("------------------------------Plotting " + key + "...")          
+      plot_data(key, data, type_)
       
