636d324ff223bb3a4eb039ab9cefb39d2ad3fdc7
Alexandra Nava <aenava@ucsc.edu>
2024-01-22 18:56:37 -0800

    Have data for both iphone and device

--- concatenate_csvs.py
+++ concatenate_csvs.py
@@ -15,37 +15,42 @@ def correct_naming(path):
 
 if __name__ == "__main__":
   global base
-  type_ = "Device"
-  base = "/Users/alexandranava/Desktop/DARPA/Tasks/Count_Control_Images/Count Data/V2/"
-  output_path = base + "Data_V2.csv"
+  type_ = "iPhone"
+  base = "/Users/alexandranava/Desktop/DARPA/Tasks/Count_Control_Images/Count Data/V1/"
+
+  output_path = base + type_ + "_Data_V2.csv"
   output_path_V2 = "/Users/alexandranava/Desktop/DARPA/Tasks/Count_Control_Images/Count Data/" + "Data_" + type_ + "_V3.csv"
 
   exp1_pig305 = "Exp1_Pig305"
   exp1_pig306 = "Exp1_Pig306"
-  exp4_pig1 = "Exp4_Pig1"
-  exp4_pig2 = "Exp4_Pig2"
-  exp5_pig1 = "Exp5_Pig1"
-  exp5_pig2 = "Exp5_Pig2"
-  exp6_pig1 = "Exp6_Pig1"
-  exp6_pig2 = "Exp6_Pig2"
-  exp7_pig2 = "Exp7_Pig2"
-  exp7_pig3 = "Exp7_Pig3"
+  exp2 = "Exp2"
+  exp3 = "Exp3"
+  exp4 = "Exp4"
+  exp5 = "Exp5"
+  exp6 = "Exp6"
+  exp7 = "Exp7"
   exp8 = "Exp8"
+  exp9 = "Exp9"
   exp10 = "Exp10"
   exp11 = "Exp11"
   exp12 = "Exp12"
   exp13 = "Exp13"
   exp14 = "Exp14"
+  exp15 = "Exp15"
+  exp16 = "Exp16"
+  exp17 = "Exp17"
+  exp18 = "Exp18"
+  exp19 = "Exp19"
 
-  ind_files = [exp1_pig305, exp1_pig306, exp4_pig1, exp4_pig2, exp5_pig1, exp5_pig2, exp6_pig1, exp6_pig2, exp7_pig2, exp7_pig3, exp8, exp10, exp11, exp12, exp13]
+  ind_files = [exp1_pig305, exp1_pig306, exp2, exp3, exp4, exp5, exp6, exp7, exp8, exp9, exp10, exp11, exp12, exp13, exp14, exp15, exp16, exp17, exp18, exp19]
   main_df = pd.DataFrame()
   for index in range(0,len(ind_files)-1):
     file = ind_files[index]
-    file_path = base + file + ".csv"
+    file_path = base + type_ + "_" + file + ".csv"
     df_curr = pd.read_csv(file_path)
     main_df = pd.concat([main_df, df_curr], axis=0, ignore_index=True)
 
-  main_df.to_csv(output_path, header = ["Dropbox Path", "Experiment", "Wound", "Folder Name", "Count"])
+  main_df.to_csv(output_path, header = ["Index", "Dropbox Path", "Experiment", "Wound", "Folder Name", "Count"])
   print("Output V2...")
   main_df_updated = correct_naming(output_path)
   print("Updated naming...")
